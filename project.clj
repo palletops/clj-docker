944e5d7d213d0cb0376401ec41c03ab74f3eb2f3
Hugo Duncan <hugo@palletops.com>
2014-07-15 16:18:57 +0000

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/clj-docker "0.1.1"
+(defproject com.palletops/clj-docker "0.1.2-SNAPSHOT"
   :description "A clojure wrapper for the Docker API"
   :url "https://github.com/palletops/clj-docker"
   :license {:name "Eclipse Public License"
